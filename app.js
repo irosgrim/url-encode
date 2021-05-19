189e5b50a67a4af8b0284dc2c4cc9265b1a06ea9
ion <irosgrim@gmail.com>
2021-05-19 11:05:27 +0200

    url encode links

---
+++ app.js
@@ -0,0 +1,28 @@
+const input = document.getElementById('input');
+const output = document.getElementById('output');
+const convertBtns = document.getElementById('convert-btns');
+
+convertBtns.addEventListener('click', onConvert);
+
+function onConvert(e) {
+    const convertTo = e.target.dataset.convert
+    output.value = '';
+    if(input.value) {
+        const urls = input.value.split('\n').filter(x => x);
+        switch (convertTo) {
+            default:
+            case 'SV':
+                for(const url of urls) {
+                    output.value += "https://whitelabel.sembo.se/Redirecting.aspx?page=" +encodeURIComponent(url);
+                    output.value += "\n\n-------------\n\n"
+                }
+                return
+            case 'DK':
+                for(const url of urls) {
+                    output.value += "https://whitelabel.dtf-travel.dk/Redirecting.aspx?page=" +encodeURIComponent(url);
+                    output.value += "\n\n-------------\n\n"
+                }
+                return
+        }
+    }
+}
